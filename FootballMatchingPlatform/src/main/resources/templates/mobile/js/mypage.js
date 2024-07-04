872769015ac994eff820ec0f737aac410651abe6
migang <mig.sooon@gmail.com>
2024-07-04 15:15:46 +0900

    feat: 마이페이지 - 내 정보 수정 구현중, 마이페이지 뒤로가기 용 헤더 생성

--- FootballMatchingPlatform/src/main/resources/templates/mobile/js/mypage.js
+++ FootballMatchingPlatform/src/main/resources/templates/mobile/js/mypage.js
@@ -4,12 +4,39 @@ $("header").on("click", "#logout", function () {
      alert('로그아웃')
 });
 
-// 정보 수정 페이지 이동
+// 정보 수정 - 비밀번호 확인 페이지 이동
 $("#edit-info").on("click", function () {
-    alert('정보 수정 페이지 이동')
+    alert('정보 수정 - 비밀번호 확인 페이지 이동')
 });
 
 // 나의 문의 페이지 이동
 $("#more-inquiry").on("click", function () {
     alert('나의 문의 페이지 이동')
+});
+
+
+
+////////////////////// 내 정보 수정 //////////////////////////////
+
+
+// 정보 수정 - 정보 수정 페이지 이동
+$("#check-pw-btn").on("click", function () {
+    alert('비밀번호 확인 메서드 실행 , 모달창 또는 정보 수정 - 정보 수정 페이지 이동')
+});
+
+// 회원 정보 수정 폼
+
+// 비밀번호 수정 버튼 클릭 시
+$("#edit-pw-btn").on("click", function () {
+    alert('비밀번호 수정 페이지로 이동')
+});
+
+// 회원 탈퇴 버튼 클릭 시
+$("#withdraw-btn").on("click", function () {
+    alert('회원 탈퇴 페이지로 이동')
+});
+
+// 수정하기 버튼 클릭 시
+$("#my-info-edit-btn").on("click", function () {
+    alert('회원 정보 수정 메소드 실행')
 });
