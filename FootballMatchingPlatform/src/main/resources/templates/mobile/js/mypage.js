eb49d1334f3cf059ede77a27b7203bef3d7ad10c
migang <mig.sooon@gmail.com>
2024-07-04 12:14:56 +0900

    feat: mypage 메인 페이지 구현 완료, 마이페이지용 헤더 생성

---
+++ FootballMatchingPlatform/src/main/resources/templates/mobile/js/mypage.js
@@ -0,0 +1,15 @@
+
+// 로그아웃
+$("header").on("click", "#logout", function () {
+     alert('로그아웃')
+});
+
+// 정보 수정 페이지 이동
+$("#edit-info").on("click", function () {
+    alert('정보 수정 페이지 이동')
+});
+
+// 나의 문의 페이지 이동
+$("#more-inquiry").on("click", function () {
+    alert('나의 문의 페이지 이동')
+});
