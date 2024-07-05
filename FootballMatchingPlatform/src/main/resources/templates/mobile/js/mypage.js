bf4911fda32d23b5476998ee2832bd0e4d62214e
migang <mig.sooon@gmail.com>
2024-07-05 17:52:19 +0900

    feat: 마이페이지 구현 완료, css 일부 수정

--- FootballMatchingPlatform/src/main/resources/templates/mobile/js/mypage.js
+++ FootballMatchingPlatform/src/main/resources/templates/mobile/js/mypage.js
@@ -32,8 +32,8 @@ $("#edit-pw-btn").on("click", function () {
 });
 
 // 회원 탈퇴 버튼 클릭 시
-$("#withdraw-btn").on("click", function () {
-    alert('회원 탈퇴 페이지로 이동')
+$("#withdrawModal-btn").on("click", function () {
+    alert('회원 탈퇴 메소드 실행')
 });
 
 // 수정하기 버튼 클릭 시
@@ -41,6 +41,23 @@ $("#my-info-edit-btn").on("click", function () {
     alert('회원 정보 수정 메소드 실행')
 });
 
+
+// 회원 아이디, 이메일 확인 페이지
+
+$('#check-id-email-btn').on('click', function () {
+    const userId = $("#user-id").val();
+    const userEmail = $("#user-email").val();
+
+    alert(userId)
+    alert(userEmail)
+    alert('아이디, 이메일 확인 메소드')
+});
+
+// 비밀번호 수정 페이지
+// 비밀번호 수정 버튼 클릭 시
+$("#edit-pw-edit-btn").on("click", function () {
+    alert('회원 비밀번호 수정 메소드 실행')
+});
 
 
 
@@ -55,4 +72,16 @@ $('#add-inquiry-btn').on('click', function () {
 // 문의 상세 페이지 이동
 $('#container').on('click', '.inquiry-detail', function () {
     alert('문의 상세 페이지 이동')
+});
+
+
+// 문의 작성 페이지
+// 등록하기 버튼 클릭 시
+$('#addinquiry-btn').on('click', function () {
+    const inquiryTitle = $("#addinquiry-title-input").val();
+    const inquiryContent = $("#addinquiry-content-input").val();
+
+    alert(inquiryTitle)
+    alert(inquiryContent)
+    alert('문의 등록')
 });
