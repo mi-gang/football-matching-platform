0841030e9c290b2b2846db1b5489748addf49390
migang <mig.sooon@gmail.com>
2024-07-09 15:44:33 +0900

    feat: matching repository 생성중

---
+++ FootballMatchingPlatform/src/main/java/com/kosta/project/repository/MatchingMapper2.java
@@ -0,0 +1,35 @@
+package com.kosta.project.repository;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.kosta.project.dto.MatchingScheduleListDTO;
+import com.kosta.project.dto.UserMatchingInfoDTO;
+import com.kosta.project.dto.UserPlayInfoDTO;
+
+@Mapper
+public interface MatchingMapper2 {
+	
+	Map<Integer, LocalDate> selectMatchingListByMonth(String userId, int month);
+	Collection<MatchingScheduleListDTO> selectMatchingListByDate(String userId, int date);
+	
+	int selectMatchingListCount(String userId);
+	Collection<MatchingScheduleListDTO> selectMatchingList(String userId);
+	boolean updatePayStatus(UserMatchingInfoDTO userMatchingInfoDTO); // String userId, int matchingSeq
+	boolean deleteMatching(UserMatchingInfoDTO userMatchingInfoDTO); // String userId, int matchingSeq
+	
+	// 상대팀 평가
+	Collection<UserPlayInfoDTO> selectOpposingTeamPlayerList (UserMatchingInfoDTO userMatchingInfoDTO); // String userId, int matchingSeq
+	boolean updateReviewScore(UserMatchingInfoDTO userMatchingInfoDTO); // String userId, int matchingSeq, int score
+	
+	
+	Collection<UserPlayInfoDTO> selectPlayerList(int matchingSeq);
+	
+	// 점수 확인
+	int selectReviewScore(UserMatchingInfoDTO userMatchingInfoDTO); // String userId, int matchingSeq
+	int selectTeamScore(UserMatchingInfoDTO userMatchingInfoDTO); // String userId, int matchingSeq
+	
+}
