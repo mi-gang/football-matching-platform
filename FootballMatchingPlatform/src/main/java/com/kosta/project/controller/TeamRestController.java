38d40380d5fe40005727cc8e33d6fb56fa7041af
MyeoungJu <fkfk1102@gmail.com>
2024-07-12 15:03:45 +0900

    feat : team controller 진행중3

--- FootballMatchingPlatform/src/main/java/com/kosta/project/controller/TeamRestController.java
+++ FootballMatchingPlatform/src/main/java/com/kosta/project/controller/TeamRestController.java
@@ -3,13 +3,19 @@ package com.kosta.project.controller;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.thymeleaf.TemplateEngine;
 
 import com.kosta.project.dto.TeamDTO;
 import com.kosta.project.service.TeamService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -19,8 +25,13 @@ public class TeamRestController {
 	
 	@GetMapping("/rank")
 	public Map<String, List<TeamDTO>> getTeamRankList(){
-		
 		return Map.of("result", ts.getTeamRankList());
 	}
+
+	@GetMapping("/possJoin")
+	public Map<String, List<TeamDTO>> getPossibleJoinTeam(){
+		return Map.of("result", ts.getPossibleJoinTeam());
+	}
 	
+
 }
