dd71790964694b01b966cea329c89fcf2937d364
MyeoungJu <fkfk1102@gmail.com>
2024-07-16 18:13:31 +0900

    feat : test 주석처리

--- FootballMatchingPlatform/src/test/java/com/kosta/project/repository/MatchingMapperTestJuho.java
+++ FootballMatchingPlatform/src/test/java/com/kosta/project/repository/MatchingMapperTestJuho.java
@@ -16,20 +16,20 @@ public class MatchingMapperTestJuho {
 	MatchingMapper mm;
 	
 	
-	// 다가오는 경기 일정을 불러옵니다.
-	@Test
-	void selectMatchingAlreadyTest() {
-		String userId = "user00100";
-		List<MatchingDTO> ml = mm.selectMatchingAlready(userId);
-		for (MatchingDTO matchingDTO : ml) {
-            System.out.println("매칭 상태 : " + matchingDTO.getMatchingStatus()
-            					+ " 매칭 날짜 : " + matchingDTO.getMatchingDate()
-            					+ " 매칭 시간대 : " + matchingDTO.getMatchingTime()
-            					+ " 구장 위치 : " + matchingDTO.getFeildAddress()
-            					+ " 구장 위치 상세 : " + matchingDTO.getFeildAddressDetail()
-            					+ " 구장 명 : " + matchingDTO.getFeildName());
-        }
-	}
-	
+//	// 다가오는 경기 일정을 불러옵니다.
+//	@Test
+//	void selectMatchingAlreadyTest() {
+//		String userId = "user00100";
+//		List<MatchingDTO> ml = mm.selectMatchingAlready(userId);
+//		for (MatchingDTO matchingDTO : ml) {
+//            System.out.println("매칭 상태 : " + matchingDTO.getMatchingStatus()
+//            					+ " 매칭 날짜 : " + matchingDTO.getMatchingDate()
+//            					+ " 매칭 시간대 : " + matchingDTO.getMatchingTime()
+//            					+ " 구장 위치 : " + matchingDTO.getFeildAddress()
+//            					+ " 구장 위치 상세 : " + matchingDTO.getFeildAddressDetail()
+//            					+ " 구장 명 : " + matchingDTO.getFeildName());
+//        }
+//	}
+//	
 	
 }
