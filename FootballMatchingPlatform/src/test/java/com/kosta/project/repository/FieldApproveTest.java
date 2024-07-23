5ceec1fb7227bb261fae9a7804ffd96c5bfae9f2
mi-gang <mig.sooon@gmail.com>
2024-07-23 11:07:52 +0900

    Revert "feat: 오류 수정중"
    This reverts commit ac32454a3bdf02788882b2d62d0d5822f90489cf.

--- FootballMatchingPlatform/src/test/java/com/kosta/project/repository/FieldApproveTest.java
+++ FootballMatchingPlatform/src/test/java/com/kosta/project/repository/FieldApproveTest.java
@@ -13,11 +13,11 @@ import com.kosta.project.domain.Field;
 
 @SpringBootTest
 public class FieldApproveTest {
-
+	
 	@Autowired
 	FieldRepository fr;
-
-
+	
+	
 	// 구장 승인 (승인 날짜 업데이트)
 	//@Test
 	public void updateApproveTest() {
@@ -25,26 +25,28 @@ public class FieldApproveTest {
 		Date date = new Date();
 		Instant instant = date.toInstant();
 		LocalDate localDate = instant.atZone(ZoneId.systemDefault()).toLocalDate();
-
-		// 	field.setFieldApprovalDate(localDate);
-
-		// 	fr.save(field);
-
-		// }
-
-
-		// 구장 승인 거절(field_status를 2로 변경)
-//	@Test
-//	public void updateFieldStatus2Test() {
-//		Field field = fr.findById(8).get();
-
-		// 	int status = 2;
-
-		// 	field.setFieldStatus(status);
-
-		// 	fr.save(field);
-		// }
-
-
-	}
+		
+	// 	field.setFieldApprovalDate(localDate);
+		
+	// 	fr.save(field);
+		
+	 }
+	
+	
+	// 구장 승인 거절(field_status를 2로 변경)
+	@Test
+	public void updateFieldStatus2Test() {
+		Field field = fr.findById(8).get();
+		
+	// 	int status = 2;
+		
+	// 	field.setFieldStatus(status);
+		
+	// 	fr.save(field);
+	 }
+	
+	
+	
+	
+	
 }
