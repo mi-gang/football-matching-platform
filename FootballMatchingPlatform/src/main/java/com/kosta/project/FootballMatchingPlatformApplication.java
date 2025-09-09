410dc0ee679f5afdab4c59e11ac4fddf9d322b37
mi-gang <mig.sooon@gmail.com>
2025-09-09 14:09:28 +0900

    init: personal clone of futsal project

---
+++ FootballMatchingPlatform/src/main/java/com/kosta/project/FootballMatchingPlatformApplication.java
@@ -0,0 +1,14 @@
+package com.kosta.project;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballMatchingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballMatchingPlatformApplication.class, args);
+	}
+
+}
---
+++ FootballMatchingPlatform/src/test/java/com/kosta/project/FootballMatchingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosta.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballMatchingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
